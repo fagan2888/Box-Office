84c5d619297ac562539d46bfe1b679bcac04df66
Rebecca George <rebeccageorge2011@gmail.com>
2018-06-08 19:04:35 -0400

    Update pull_OMDB_API_newMovies.py

--- codes/API_pulls/pull_OMDB_API_newMovies.py
+++ codes/API_pulls/pull_OMDB_API_newMovies.py
@@ -25,7 +25,7 @@ movies = pd.read_csv(r'c:\users\rebecca\desktop\movies\2018 Movies\2017_movies_o
 #for this to run without errors. He asked that I not share that
 #URL. As a result, what I provide here will likely time out for you.
 #url = 'http://www.omdbapi.com/?t='
-url_p1 = 'http://108.60.201.28/?t='  #can't provide his direct server URL address
+url_p1 = 'http://---.--.---.--/?t='  #can't provide his direct server URL address
 url_p2 = '&y='
 api_key = '&apikey=df2ef738'
 
